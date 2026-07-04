409c51e61bf0c7e953cd3d9c5134ce8ee1b80409
liberty212 <truth20021@gmail.com>
2026-07-04 13:35:31 +0000

    Update domain links

--- shared/utils/helpers.ts
+++ shared/utils/helpers.ts
@@ -1,13 +1,13 @@
 export const RESOURCES = {
   apiV2: "https://kick.com/api/v2",
   clipsTmp: "https://clips.kick.com/tmp",
-  worker: "https://dev.ahmedrangel.com",
-  cdn: "https://cdn.ahmedrangel.com/tmp/videos/kick"
+  worker: "https://www.grabkickclips.com",
+  cdn: "https://www.grabkickclips.com/tmp/videos/kick"
 };
 
 export const SITE = {
   name: "KickClips",
-  url: "https://kickclips.ahmedrangel.com",
+  url: "https://www.grabkickclips.com/",
   description: "KickClips is a free online tool for downloading MP4 clips from kick.com.",
   title: "KickClips | Downloader | to MP4",
   userAgent: "KickClips (Cloudflare Workers)"
