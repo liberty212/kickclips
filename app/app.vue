0a48502b70f0663b8717b299381554401d056f49
liberty212 <truth20021@gmail.com>
2026-07-07 17:42:10 +0000

    Add SEO meta tags

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+useSeoMeta({
+ title: 'Kick.com Clip Downloader | Save & Grab Clips Fast',
+ description: 'The fastest way to download clips from Kick.com. Easily save, grab, and share your favorite Kick stream highlights in high quality.'
+})
+</script>
+
 <template>
-  <div>
-    <NuxtLoadingIndicator :throttle="0" />
-    <BackToTop />
-    <NuxtPage />
-  </div>
+ <div>
+ <NuxtLoadingIndicator :throttle="0" />
+ <BackToTop />
+ <NuxtPage />
+ </div>
 </template>
